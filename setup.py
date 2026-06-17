f6927859a25ab80f26fe71faad094bf186f04dd6
MALISH-WEB <12345678+MALISH-WEB@users.noreply.github.com>
2026-06-17 23:01:03 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="malish abui",
+    author_email="malishemmanuelabui17@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
